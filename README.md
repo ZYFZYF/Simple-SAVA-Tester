82137cd7cc83d9fbca9f894ccb98a69b9368dab1
ZYFZYF <973446154@qq.com>
2020-11-13 09:14:20 +0800

    添加测试脚本

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Simple-SAVA-Tester
+
+## 想法
+ - 能不能在路径上用client对某个路由器模拟TCP三次握手，在即将要完成的时候把让server伪造client来发最后一个包，然后client用某种行为来验证与路由器的tcp连接已经被建立了
+    - 要么是重新发送这个包，看那边会回复什么？
+    - 要么是直接来close掉这个连接
+ - 不用那么麻烦，ping包的reply包是真实的，所以直接用伪造源地址ping那台设备就可以看看这台设备有没有收到这个包了
+ 
+## TODO
+ - traceroute的时候有空缺的跳数似乎？
+    - ![traceroute](traceroute.png)
---
+++ test.py
@@ -0,0 +1,44 @@
+from scapy.all import *
+from scapy.layers.inet6 import IPv6, UDP, Ether, traceroute6
+from scapy.as_resolvers import AS_resolver_radb
+import time
+
+
+def test_send_ethernet():
+    B = '2001:da8:ff:212::41:23'
+    for i in range(100):
+        a = Ether(src='aa:bb:cc:dd:ee:dd') / IPv6(dst=B) / UDP(sport=52984, dport=9877) / 'QAQQ'
+        time.sleep(1)
+        sendp(a)
+
+
+def test_send():
+    B = '2001:da8:ff:212::41:23'
+    for i in range(100):
+        a = IPv6(src='1111:2222:3:4444:5555:6666:7777:8888', dst=B) / UDP(sport=52984, dport=9877) / 'QAQQ'
+        time.sleep(1)
+        send(a)
+
+
+def callback(packet):
+    packet.show()
+
+
+def test_recv():
+    sniff(filter="host 2001:da8:ff:212::41:23 and udp", count=2, timeout=5, prn=callback)
+
+
+def test_trace_route():
+    a, b = traceroute6('2001:da8:ff:212::41:23')
+    print(a.get_trace())
+    a.graph(target='>test.png', ASres=AS_resolver_radb(), type='png')
+    w = sorted(a.get_trace()['2001:da8:ff:212::41:23'].items(), key=lambda x: x[0])
+    print([i[1][0] for i in w])
+
+
+if __name__ == '__main__':
+    # test_send_ethernet()
+    # test_send()
+    # send(IPv6(src='2402:f000:2:4001:4c0:f05d:b450:b37a', dst='2001:da8:ff:212::41:23') / UDP(sport=12345,
+    #                                                                                          dport=9877) / 'Test')
+    test_trace_route()
